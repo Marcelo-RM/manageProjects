4962a33c43f81c7f32c736ce8824dc87878fa2a5
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-06-27 19:42:13 -0300

    começo navigation

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,20 @@
 sap.ui.define([
+	"sap/ui/core/UIComponent",
 	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+], function (UIComponent, Controller) {
 	"use strict";
 
 	return Controller.extend("manageprojects.manageprojects.controller.App", {
 		onInit: function () {
+
+		},
+
+		selectProject: function(oEvent) {
+			debugger;
+			var idProject = oEvent.getSource().getProperty("number");
+
+			this.oRouter = UIComponent.getRouterFor(this);
+			this.oRouter.navTo("detail", {idProject});
 
 		}
 	});
